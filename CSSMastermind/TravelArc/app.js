4dd252458d340d247c2f4f69f7102a74865bd00c
MHC32 <hantzmichaelchery6@gmail.com>
2023-06-26 01:55:50 -0400

    start navbar

--- CSSMastermind/TravelArc/app.js
+++ CSSMastermind/TravelArc/app.js
@@ -1,1 +1,13 @@
-console.log("Hello MHC32")
+// Sélectionne tous les liens dans la liste
+const links = document.querySelectorAll('.container-navbar ul li a');
+
+// Parcourt tous les liens
+links.forEach(link => {
+  // Ajoute un écouteur d'événement 'click' à chaque lien
+  link.addEventListener('click', function() {
+    // Supprime la classe 'active' de tous les liens
+    links.forEach(link => link.classList.remove('active'));
+    // Ajoute la classe 'active' uniquement au lien cliqué
+    this.classList.add('active');
+  });
+});
