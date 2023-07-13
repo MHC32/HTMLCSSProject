7cd5f8f0140b8d8994d41c887303255cb5a027fd
MHC32 <hantzmichaelchery6@gmail.com>
2023-07-13 03:42:29 -0400

    new roject otakusocial

--- CSSMastermind/neckoSocial/app.js
+++ CSSMastermind/neckoSocial/app.js
@@ -1,1 +1,50 @@
-alert()
+// Sidebar 
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+
+// Messages 
+const messagesNotification = document.querySelector("#messages-notification");
+const messages = document.querySelector(".messages");
+
+
+
+// Remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    });
+};
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id !== 'notifications') {
+            document.querySelectorAll('.notification-popup').forEach(popup => {
+                popup.style.display = "none";
+            });
+        } else {
+            document.querySelectorAll('.notification-popup').forEach(popup => {
+                popup.style.display = "block";
+            });
+            document.querySelectorAll('#notifications .notification-count').forEach(count => {
+                count.style.display = 'none';
+            });
+        }
+    });
+});
+
+
+
+// Messages 
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelectorAll('.notification-count').forEach(count => {
+        count.style.display = 'none';
+    });
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+});
