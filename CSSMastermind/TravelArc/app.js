f74bd2814264b9a2e8dbcb7e7100f7ec428ceabf
MHC32 <hantzmichaelchery6@gmail.com>
2023-07-14 13:56:20 -0400

    travel-arc started

--- CSSMastermind/TravelArc/app.js
+++ CSSMastermind/TravelArc/app.js
@@ -1,31 +1,4 @@
-// Sélectionne tous les liens dans la liste
-const links = document.querySelectorAll('nav > ul li a');
-
-// Parcourt tous les liens
-links.forEach(link => {
-  // Ajoute un écouteur d'événement 'click' à chaque lien
-  link.addEventListener('click', function() {
-    // Supprime la classe 'active' de tous les liens
-    links.forEach(link => link.classList.remove('active'));
-    // Ajoute la classe 'active' uniquement au lien cliqué
-    this.classList.add('active');
-  });
-});
-
-
-// Sélectionne tous les liens dans la liste
-const buttons = document.querySelectorAll('.container-button button');
-
-// Parcourt tous les liens
-links.forEach(button => {
-  // Ajoute un écouteur d'événement 'click' à chaque lien
-  button.addEventListener('click', function() {
-    // Supprime la classe 'active' de tous les liens
-    links.forEach(button => button.classList.remove('active'));
-    // Ajoute la classe 'active' uniquement au lien cliqué
-    this.classList.add('active');
-  });
-});
+console.log('Hello World')
 
 
 
